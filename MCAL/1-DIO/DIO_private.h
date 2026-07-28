861d374081534b5ee9624383e88adee21adcdd2f
RipLinux <anassayedaliali19_sd25@eng.bsu.edu.eg>
2026-07-28 22:35:36 +0300

    Add DIO_private.h with port register definitions
    Define registers for DIO ports A, B, C, and D.

---
+++ MCAL/1-DIO/DIO_private.h
@@ -0,0 +1,26 @@
+/***************************************/
+/******   Author:   Anas         *******/
+/******   Time:     07/28/2026   *******/
+/******   File:     Private File *******/
+/***************************************/
+#ifndef DIO_PRIVATE_H
+#define DIO_PRIVATE_H
+
+/* Port A Registers */
+#define DDRA *((volatile u8 *)0x3A)
+#define PORTA *((volatile u8 *)0x3B)
+#define PINA *((volatile u8 *)0x39)
+/* Port B Registers */
+#define DDRB *((volatile u8 *)0x37)
+#define PORTB *((volatile u8 *)0x38)
+#define PINB *((volatile u8 *)0x36)
+/* Port C Registers */
+#define DDRC *((volatile u8 *)0x34)
+#define PORTC *((volatile u8 *)0x35)
+#define PINC *((volatile u8 *)0x33)
+/* Port D Registers */
+#define DDRD *((volatile u8 *)0x31)
+#define PORTD *((volatile u8 *)0x32)
+#define PIND *((volatile u8 *)0x30)
+
+#endif /*DIO_PRIVATE_H*/
