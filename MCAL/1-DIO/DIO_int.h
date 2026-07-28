2e6910da3909f8dbe6aa3c1ba4fb40899c4ea01e
RipLinux <anassayedaliali19_sd25@eng.bsu.edu.eg>
2026-07-28 22:34:46 +0300

    Add DIO interface header file

---
+++ MCAL/1-DIO/DIO_int.h
@@ -0,0 +1,38 @@
+/***************************************/
+/******   Author:   Anas         *******/
+/******   Time:     07/28/2026   *******/
+/******   File:   Interface File *******/
+/***************************************/
+#ifndef DIO_INT_H
+#define DIO_INT_H
+
+#define PORTA 0
+#define PORTB 1
+#define PORTC 2
+#define PORTD 3
+
+#define PIN0 0
+#define PIN1 1
+#define PIN2 2
+#define PIN3 3
+#define PIN4 4
+#define PIN5 5
+#define PIN6 6
+#define PIN7 7
+
+#define OUTPUT 1
+#define INPUT 0
+
+#define LOW 0
+#define HIGH 1
+
+void DIO_voidInitialization(void);
+void DIO_voidSetPinDirection(u8 PortID,u8 PinID,u8 Direction);
+void DIO_voidSetPinValue(u8 PortID,u8 PinID,u8 Value);
+u8 DIO_u8GetPinValue(u8 PortID,u8 PinID);
+void DIO_voidTogPinValue(u8 PortID,u8 PinID);
+void DIO_voidSetPortDirection(u8 PortID,u8 Direction);
+void DIO_voidSetPortValue(u8 PortID,u8 Value);
+
+
+#endif /*DIO_INT_H*/
