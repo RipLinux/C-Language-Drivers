a726c6de1b81fb548947bca148a39a495f232d07
RipLinux <anassayedaliali19_sd25@eng.bsu.edu.eg>
2026-07-29 23:27:37 +0300

    Fix include statements and update register names

--- MCAL/1-DIO/DIO_prog.c
+++ MCAL/1-DIO/DIO_prog.c
@@ -1,14 +1,14 @@
 /***************************************/
-/******   Author:   Anas         *******/
+/******   Author:   Anas  Sayed  *******/
 /******   Time:     07/28/2026   *******/
 /******   File:     Program File *******/
 /***************************************/
-#include <Std_Types.h>
-#include <Bit_Math.h>
+#include "Std_Types.h"
+#include "Bit_Math.h"
 
-#include <DIO_private.h>
-#include <DIO_int.h>
-#include <DIO_config.h>
+#include "DIO_private.h"
+#include "DIO_int.h"
+#include "DIO_config.h"
 
 void DIO_voidInitialization(void)
 {
@@ -20,7 +20,7 @@ void DIO_voidInitialization(void)
 
 void DIO_voidSetPinDirection(u8 PortID,u8 PinID,u8 Direction)
 {
-    if(( PortID < 4 ) && ( PinID < 8 ) && (Direction == OUTPUT || Direction == INPUT )) 
+    if(( PortID < 4 ) && ( PinID < 8 ) && (Direction == OUTPUT || Direction == INPUT ))
     {
         if(Direction == OUTPUT)
         {
@@ -45,110 +45,112 @@ void DIO_voidSetPinDirection(u8 PortID,u8 PinID,u8 Direction)
     }
     else
     {
-    
+
     }
 }
 
 void DIO_voidSetPinValue(u8 PortID,u8 PinID,u8 Value)
 {
-    if(( PortID < 4 ) && ( PinID < 8 ) && (Value == HIGH || Value == LOW )) 
+    if(( PortID < 4 ) && ( PinID < 8 ) && (Value == HIGH || Value == LOW ))
     {
         if(Value == HIGH)
         {
             switch(PortID)
             {
-                case PORTA: SET_BIT(PORTA, PinID); break;
-                case PORTB: SET_BIT(PORTB, PinID); break;
-                case PORTC: SET_BIT(PORTC, PinID); break;
-                case PORTD: SET_BIT(PORTD, PinID); break;
+                case PORTA: SET_BIT(PORTA_REG, PinID); break;
+                case PORTB: SET_BIT(PORTB_REG, PinID); break;
+                case PORTC: SET_BIT(PORTC_REG, PinID); break;
+                case PORTD: SET_BIT(PORTD_REG, PinID); break;
             }
         }
         else if(Value == LOW)
         {
             switch(PortID)
             {
-            case PORTA: CLR_BIT(PORTA, PinID); break;
-            case PORTB: CLR_BIT(PORTB, PinID); break;
-            case PORTC: CLR_BIT(PORTC, PinID); break;
-            case PORTD: CLR_BIT(PORTD, PinID); break;
+            case PORTA: CLR_BIT(PORTA_REG, PinID); break;
+            case PORTB: CLR_BIT(PORTB_REG, PinID); break;
+            case PORTC: CLR_BIT(PORTC_REG, PinID); break;
+            case PORTD: CLR_BIT(PORTD_REG, PinID); break;
             }
         }
     }
     else
     {
-    
+
     }
 }
 
 u8 DIO_u8GetPinValue(u8 PortID,u8 PinID)
 {
-    if(( PortID < 4 ) && ( PinID < 8 )) 
+    u8 Local_u8PinValue = 0;
+
+    if(( PortID < 4 ) && ( PinID < 8 ))
     {
          switch(PortID)
         {
-            case PORTA: x=GET_BIT(PORTA, PinID); break;
-            case PORTB: x=GET_BIT(PORTB, PinID); break;
-            case PORTC: x=GET_BIT(PORTC, PinID); break;
-            case PORTD: x=GET_BIT(PORTD, PinID); break;
+            case PORTA: Local_u8PinValue = GET_BIT(PORTA_REG, PinID); break;
+            case PORTB: Local_u8PinValue = GET_BIT(PORTB_REG, PinID); break;
+            case PORTC: Local_u8PinValue = GET_BIT(PORTC_REG, PinID); break;
+            case PORTD: Local_u8PinValue = GET_BIT(PORTD_REG, PinID); break;
         }
     }
     else
     {
-    
+
     }
-    return x;
+    return Local_u8PinValue;
 }
 
 void DIO_voidTogPinValue(u8 PortID,u8 PinID)
 {
-    if(( PortID < 4 ) && ( PinID < 8 )) 
+    if(( PortID < 4 ) && ( PinID < 8 ))
     {
          switch(PortID)
         {
-            case PORTA: x=TOG_BIT(PORTA, PinID); break;
-            case PORTB: x=TOG_BIT(PORTB, PinID); break;
-            case PORTC: x=TOG_BIT(PORTC, PinID); break;
-            case PORTD: x=TOG_BIT(PORTD, PinID); break;
+            case PORTA: TOG_BIT(PORTA_REG, PinID); break;
+            case PORTB: TOG_BIT(PORTB_REG, PinID); break;
+            case PORTC: TOG_BIT(PORTC_REG, PinID); break;
+            case PORTD: TOG_BIT(PORTD_REG, PinID); break;
         }
     }
     else
     {
-    
+
     }
 }
 
 void DIO_voidSetPortDirection(u8 PortID,u8 Direction)
 {
-    if(( PortID < 4 ) && (Direction == OUTPUT || Value == INPUT )) 
+    if(( PortID < 4 ) && (Direction == OUTPUT || Direction == INPUT ))
     {
          switch(PortID)
         {
             case PORTA: DDRA = Direction; break;
             case PORTB: DDRB = Direction; break;
             case PORTC: DDRC = Direction; break;
-            case PORTD: DDR = Direction; break;
+            case PORTD: DDRD = Direction; break;
         }
     }
     else
     {
-    
+
     }
 }
 
 void DIO_voidSetPortValue(u8 PortID,u8 Value)
 {
-    if(( PortID < 4 ) && (Value == HIGH || Value == LOW ) ) 
+    if(( PortID < 4 ) && (Value == HIGH || Value == LOW ) )
     {
          switch(PortID)
         {
-            case PORTA: PORTA = Value; break;
-            case PORTB: PORTB = Value; break;
-            case PORTC: PORTC = Value; break;
-            case PORTD: PORTD = Value; break;
+            case PORTA: PORTA_REG = Value; break;
+            case PORTB: PORTB_REG = Value; break;
+            case PORTC: PORTC_REG = Value; break;
+            case PORTD: PORTD_REG = Value; break;
         }
     }
     else
     {
-    
+
     }
 }
