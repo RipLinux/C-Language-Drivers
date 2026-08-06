007bfbc513ac9767e1d4438ef4e14d262b210c67
RipLinux <anassayedaliali19_sd25@eng.bsu.edu.eg>
2026-08-06 15:24:26 +0300

    Add files via upload

---
+++ HAL/2_KEYPAD/KeyPad_int.h
@@ -0,0 +1,17 @@
+/***************************************/
+/******   Author:   Anas  Sayed  *******/
+/******   File:   keypad driver  *******/
+/***************************************/
+
+#ifndef HAL_KEYPAD_KEYPAD_H_
+#define HAL_KEYPAD_KEYPAD_H_
+
+#include "../../MCAL/Dio/Dio.h"
+#include "KeyPad_Config.h"
+
+
+
+uint8 KP_GetPressed();
+
+
+#endif /* HAL_KEYPAD_KEYPAD_H_ */
---
+++ HAL/3_Seven_Segment/SevSegment_int.h
@@ -0,0 +1,17 @@
+/***************************************/
+/******   Author:   Anas  Sayed  *******/
+/******   File: 7segment driver  *******/
+/***************************************/
+
+#ifndef HAL_SEVEN_SEGMENT_SEVSEGMENT_H_
+#define HAL_SEVEN_SEGMENT_SEVSEGMENT_H_
+
+#include "../../MCAL/Dio/Dio.h"
+#include "SevSegment_config.h"
+
+
+void SevSegment_Display_Number(uint8 number);
+
+void SevSegment_Display_Counter(uint8 counterUP_DOWN,uint16 delay);
+
+#endif /* HAL_SEVEN_SEGMENT_SEVSEGMENT_H_ */
