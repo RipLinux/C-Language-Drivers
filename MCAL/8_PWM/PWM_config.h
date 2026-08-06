d498853b6c3f9379cd65f0b6785008dd207aeee9
RipLinux <anassayedaliali19_sd25@eng.bsu.edu.eg>
2026-08-06 15:26:55 +0300

    Add files via upload

---
+++ MCAL/8_PWM/PWM_config.h
@@ -0,0 +1,15 @@
+/***************************************/
+/******   Author:   Anas  Sayed  *******/
+/******   File:    PWM   driver  *******/
+/***************************************/
+
+
+
+
+
+#ifndef TIM0_CONFIG_H_
+#define TIM0_CONFIG_H_
+
+#define TIM0_PRESCALER    PRESCALER_8
+
+#endif
